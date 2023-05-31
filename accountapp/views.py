c71a03a7b3a3ae752872cf143c22e9b353f39e37
arron-k <50351696+arron-k@users.noreply.github.com>
2023-05-31 16:41:49 +0900

    django courser 9 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(reqeust):
-    return render(reqeust, 'base.html')
+    return render(reqeust, 'accountapp/hello_world.html')
