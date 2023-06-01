5a975a23c82c632362637fd6b8b66a6bb3470398
arron-k <50351696+arron-k@users.noreply.github.com>
2023-06-01 16:13:18 +0900

    django course 17 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(reqeust):
-    return render(reqeust, 'accountapp/hello_world.html')
+    if reqeust.method == 'POST':
+        return render(reqeust, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!'})
+    else:
+        return render(reqeust, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
